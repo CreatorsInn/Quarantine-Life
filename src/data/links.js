08c35a5aba6e8f40e51e477928088721d5bffe2d
Vijay R <vijay-pt3081@csez.zohocorpin.com>
2020-04-06 21:34:56 +0530

    added:Search

--- src/data/links.js
+++ src/data/links.js
@@ -1,11 +1,39 @@
 const links = [
   {
     NAME: "Game of Thrones S01E01",
-    LINK: "download link",
+    LINK: "download link"
   },
   {
     NAME: "Game of Thrones  S01E02",
-    LINK: "download link",
+    LINK: "download link"
+  },
+  {
+    NAME: "Game of S01E01",
+    LINK: "download link"
+  },
+  {
+    NAME: "Game of S01E02",
+    LINK: "download link"
+  },
+  {
+    NAME: "Money Heist",
+    LINK: "download link"
+  },
+  {
+    NAME: "Money Heist",
+    LINK: "download link"
+  },
+  {
+    NAME: "Money Heist",
+    LINK: "download link"
+  },
+  {
+    NAME: "Money Heist",
+    LINK: "download link"
+  },
+  {
+    NAME: "Money Heist",
+    LINK: "download link"
   },
 ];
 
