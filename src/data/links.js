4ad08a40c97bf3ed9af139aec7c41d526e850d33
iCintaVj <icintavj@gmail.com>
2020-04-07 23:07:53 +0530

    ADDED: Dia

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,6 @@
 const links = [
   {
-    NAME: "Dia",
+    NAME: "Dia ( 2020 )",
     LINK:
       "magnet:?xt=urn:btih:9633D324F8E05554E05E465E52E06117C34D3EBC&dn=www.TamilRockers.ws%20-%20Dia%20%282020%29%5bKannada%20720p%20HDRip%20-%20HEVC%20-%20AC3%205.1%20-%20x265%20-%20500MB%20-%20ESubs%5d.mkv&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2fzer0day.to%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.ilibr.org%3a80%2fannounce&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2feddie4.nl%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.sktorrent.net%3a6969%2fannounce&tr=udp%3a%2f%2f9.rarbg.me%3a2720%2fannounce&tr=udp%3a%2f%2f9.rarbg.to%3a2740%2fannounce&tr=udp%3a%2f%2ftorrent.gresille.org%3a80%2fannounce&tr=udp%3a%2f%2ftracker.aletorrenty.pl%3a2710%2fannounce&tr=udp%3a%2f%2ftracker.publicbt.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.piratepublic.com%3a1337%2fannounce",
   },
@@ -10,32 +10,32 @@ const links = [
       "https://yts.mx/torrent/download/4B37249E7F82F60D94C7A15B743A6B624E640372",
   },
   {
-    NAME: "Money Heist Season 04",
+    NAME: "Money Heist Season 04 (2020)",
     LINK:
       "magnet:?xt=urn:btih:a39c33c1bd3592033cce451fc2daf76fcf887873&dn=Money+Heist+%282020%29+English+S04+Complete+720p+NF+WEBRip+DD-5.1+ES&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Money Heist Season 03",
+    NAME: "Money Heist Season 03 (2019)",
     LINK:
       "magnet:?xt=urn:btih:4a45bb7d3485ecc7c8089c5cee251e328b6f1232&dn=Money+Heist+aka+La+Casa+de+Papel+S03+Season+3+Complete+720p-XpoZ&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Money Heist Season 02",
+    NAME: "Money Heist Season 02 (2017)",
     LINK:
       "magnet:?xt=urn:btih:2277fd1e4ff702ffe382deaf53279cf4734fd4ff&dn=Money+Heist+%28La+Casa+De+Papel%29+Complete+Season+2+English+HD+720p&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Money Heist Season 01",
+    NAME: "Money Heist Season 01 (2017)",
     LINK:
       "magnet:?xt=urn:btih:a774386dfc2c48349640043b402e6a9d2e1d0e11&dn=Money+Heist+%28La+Casa+De+Papel%29+Complete+Season+1+English+HD+720p&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Game Of Thrones ( Season 01 - 07 )",
+    NAME: "Game Of Thrones ( Season 01 - 07 ) ( 2011 - 2017 )",
     LINK:
       "magnet:?xt=urn:btih:7cdb9e508c40f0a71a23f68490d32591947ab603&dn=www.TamilRockerrs.pl - Game of Thrones - Complete Season (1 to 7) - 480p English BDRip - x264 - 24GB - ESub&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce', 'extension': 'avi', 'stream': 'magnet:?xt=urn:btih:7cdb9e508c40f0a71a23f68490d32591947ab603&dn=www.TamilRockerrs.pl - Game of Thrones - Complete Season (1 to 7) - 480p English BDRip - x264 - 24GB - ESub&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce",
   },
   {
-    NAME: "Game Of Thrones Season 08",
+    NAME: "Game Of Thrones Season 08 ( 2019 )",
     LINK:
       "magnet:?xt=urn:btih:fb4b27d36048c10af9077c157b7f48ee407eb222&dn=Game Of Thrones S08 Complete Season 8 720p x264 AC3 5.1 [MKV]&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce', 'extension': 'avi', 'stream': 'magnet:?xt=urn:btih:fb4b27d36048c10af9077c157b7f48ee407eb222&dn=Game Of Thrones S08 Complete Season 8 720p x264 AC3 5.1 [MKV]&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce",
   },
