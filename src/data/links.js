0da07f13030c37395362e7ee91a9e222a6d15516
iCintaVj <icintavj@gmail.com>
2020-04-07 22:32:47 +0530

    google analytics monitoring added

--- src/data/links.js
+++ src/data/links.js
@@ -5,8 +5,9 @@ const links = [
       "https://yts.mx/torrent/download/4B37249E7F82F60D94C7A15B743A6B624E640372",
   },
   {
-    NAME: "Game of Thrones  S01E02",
-    LINK: "download link",
+    NAME: "Mayakkam Enna (2011)",
+    LINK:
+      "http://cdn22.vidorg.net/h7tocbtkamlbu3tf6rudloxq27mevrwfdsgmvnrtv2ftyb44kiq4aps4i77a/TamilYogi.com_-_Mayakkam_Enna_(2011)_Tamil_UnTouched_True_HD_1080p_AVC_6_8GB_HD_720p.mp4",
   },
   {
     NAME: "Game of S01E01",
@@ -19,7 +20,7 @@ const links = [
   {
     NAME: "Money Heist",
     LINK: "download link",
-  }
+  },
 ];
 
 export default links;
