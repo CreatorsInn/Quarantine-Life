717369e001e154b77756b0849544cdf77e5ee9b7
iCintaVj <icintavj@gmail.com>
2020-04-07 22:51:41 +0530

    ADDED:Money Heist

--- src/data/links.js
+++ src/data/links.js
@@ -5,21 +5,24 @@ const links = [
       "https://yts.mx/torrent/download/4B37249E7F82F60D94C7A15B743A6B624E640372",
   },
   {
-    NAME: "Mayakkam Enna (2011)",
+    NAME: "Money Heist Season 04",
     LINK:
-      "http://cdn22.vidorg.net/h7tocbtkamlbu3tf6rudloxq27mevrwfdsgmvnrtv2ftyb44kiq4aps4i77a/TamilYogi.com_-_Mayakkam_Enna_(2011)_Tamil_UnTouched_True_HD_1080p_AVC_6_8GB_HD_720p.mp4",
+      "magnet:?xt=urn:btih:a39c33c1bd3592033cce451fc2daf76fcf887873&dn=Money+Heist+%282020%29+English+S04+Complete+720p+NF+WEBRip+DD-5.1+ES&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Game of S01E01",
-    LINK: "download link",
+    NAME: "Money Heist Season 03",
+    LINK:
+      "magnet:?xt=urn:btih:4a45bb7d3485ecc7c8089c5cee251e328b6f1232&dn=Money+Heist+aka+La+Casa+de+Papel+S03+Season+3+Complete+720p-XpoZ&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Game of S01E02",
-    LINK: "download link",
+    NAME: "Money Heist Season 02",
+    LINK:
+      "magnet:?xt=urn:btih:2277fd1e4ff702ffe382deaf53279cf4734fd4ff&dn=Money+Heist+%28La+Casa+De+Papel%29+Complete+Season+2+English+HD+720p&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
   {
-    NAME: "Money Heist",
-    LINK: "download link",
+    NAME: "Money Heist Season 01",
+    LINK:
+      "magnet:?xt=urn:btih:a774386dfc2c48349640043b402e6a9d2e1d0e11&dn=Money+Heist+%28La+Casa+De+Papel%29+Complete+Season+1+English+HD+720p&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80",
   },
 ];
 
