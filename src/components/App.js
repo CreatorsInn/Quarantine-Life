747685ba016ddd3610fbe1f541b9096ff67faf20
iCintaVj <icintavj@gmail.com>
2020-04-07 18:37:18 +0530

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -36,33 +36,6 @@ function App() {
           autocomplete="off"
         ></input>
       </nav>
-      <main onClick={(e) => setModelOpen(false)}>
-        <div className="container">
-          {linksToShow.length === 0 ? (
-            <center>
-              <h6 class="card-title">Not Found</h6>
-            </center>
-          ) : (
-            linksToShow.map((link) => (
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="card  mt-3 mb-3">
-                    {" "}
-                    <div className="card-body">
-                      <center>
-                        <h6 class="card-title">{link.NAME}</h6>
-                        <a href={link.LINK} className="card-link">
-                          Download
-                        </a>
-                      </center>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-      </main>
       <dialog
         open={modelOpen ? true : false}
         onBlur={(e) => (e.target.open = false)}
@@ -119,6 +92,33 @@ function App() {
           </form>
         </center>
       </dialog>
+      <main onClick={(e) => setModelOpen(false)}>
+        <div className="container">
+          {linksToShow.length === 0 ? (
+            <center>
+              <h6 class="card-title">Not Found</h6>
+            </center>
+          ) : (
+            linksToShow.map((link) => (
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="card  mt-3 mb-3">
+                    {" "}
+                    <div className="card-body">
+                      <center>
+                        <h6 class="card-title">{link.NAME}</h6>
+                        <a href={link.LINK} className="card-link">
+                          Download
+                        </a>
+                      </center>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </main>
       <footer>
         <i
           className="fa fa-plus float"
