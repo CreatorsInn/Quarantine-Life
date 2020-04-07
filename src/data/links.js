dd7ef38f55b650b6924638e60878b2aa2dd19fd7
iCintaVj <icintavj@gmail.com>
2020-04-07 21:41:56 +0530

    google analytics monitoring added

--- src/data/links.js
+++ src/data/links.js
@@ -1,40 +1,25 @@
 const links = [
   {
-    NAME: "Game of Thrones S01E01",
-    LINK: "download link"
+    NAME: "Fight Club (1999)",
+    LINK:
+      "https://yts.mx/torrent/download/4B37249E7F82F60D94C7A15B743A6B624E640372",
   },
   {
     NAME: "Game of Thrones  S01E02",
-    LINK: "download link"
+    LINK: "download link",
   },
   {
     NAME: "Game of S01E01",
-    LINK: "download link"
+    LINK: "download link",
   },
   {
     NAME: "Game of S01E02",
-    LINK: "download link"
+    LINK: "download link",
   },
   {
     NAME: "Money Heist",
-    LINK: "download link"
-  },
-  {
-    NAME: "Money Heist",
-    LINK: "download link"
-  },
-  {
-    NAME: "Money Heist",
-    LINK: "download link"
-  },
-  {
-    NAME: "Money Heist",
-    LINK: "download link"
-  },
-  {
-    NAME: "Money Heist",
-    LINK: "download link"
-  },
+    LINK: "download link",
+  }
 ];
 
 export default links;
