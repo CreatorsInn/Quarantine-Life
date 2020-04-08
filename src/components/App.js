a68a4ff5aac9ae79b101add36bacf5db4ba99f3b
iCintaVj <icintavj@gmail.com>
2020-04-08 10:07:24 +0530

    UI FIX:iPhone 6

--- src/components/App.js
+++ src/components/App.js
@@ -74,20 +74,20 @@ function App() {
               onBlur={(e) => (e.target.placeholder = "Link ( if Any )")}
               autocomplete="off"
             ></input>
-            <input
+            <button
               type="submit"
               onClick={(e) => setModelOpen(false)}
               className="mx-2 mt-3"
             >
               Send
-            </input>
-            <input
+            </button>
+            <button
               type="button"
               onClick={(e) => setModelOpen(false)}
               className="mx-2 mt-3"
             >
               Close
-            </input>
+            </button>
           </form>
         </center>
       </dialog>
