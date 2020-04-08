82cfdfcf9c63d9fe9218faa9eca7d26c4efffd68
iCintaVj <icintavj@gmail.com>
2020-04-08 11:57:48 +0530

    UI FIX

--- src/components/App.js
+++ src/components/App.js
@@ -35,104 +35,112 @@ function App() {
           id="searchText"
           autocomplete="off"
         ></input>
-      </nav>
-      <dialog
-        open={modelOpen ? true : false}
-        onBlur={(e) => (e.target.open = false)}
-        id="model"
-      >
-        <center>
-          <form
-            action="https://smartforms.dev/submit/5e8c9a23d7d0665194d11f18"
-            method="POST"
-            className="form-cls"
-          >
-            <label className="mt-2 mb-2 inputWidth">
-              Have any requests ? Let us know.
-            </label>
-            <br />
-            <label className="mt-2 mb-2 inputWidth">
-              Have any Links ? Join us now.
-            </label>
-            <input
-              type="text"
-              name="media"
-              className="form-control mt-2 mb-2 inputWidth"
-              id="formGroupExampleInput"
-              placeholder="Media Name"
-              onFocus={(e) => (e.target.placeholder = "")}
-              onBlur={(e) => (e.target.placeholder = "Media Name")}
-              autocomplete="off"
-            ></input>
-            <input
-              type="text"
-              name="link"
-              className=" form-control mt-2 mb-2 inputWidth"
-              id="formGroupExampleInput"
-              placeholder="Link ( if Any )"
-              onFocus={(e) => (e.target.placeholder = "")}
-              onBlur={(e) => (e.target.placeholder = "Link ( if Any )")}
-              autocomplete="off"
-            ></input>
-            <label className="mt-2 mb-2 inputWidth">
-              Wanna be notified ? Give you mail id or Social Media Usernames.
-              W'll ping you onces you are served.
-            </label>
-            <input
-              type="text"
-              name="link"
-              className=" form-control mt-2 mb-2 inputWidth"
-              id="formGroupExampleInput"
-              placeholder="Contact Id ( Optional )"
-              onFocus={(e) => (e.target.placeholder = "")}
-              onBlur={(e) => (e.target.placeholder = "Contact Id ( Optional )")}
-              autocomplete="off"
-            ></input>
-            <button
-              type="submit"
-              onClick={(e) => setModelOpen(false)}
-              className="mx-2 mt-3"
-            >
-              Send
-            </button>
-            <button
-              type="button"
-              onClick={(e) => setModelOpen(false)}
-              className="mx-2 mt-3"
-            >
-              Close
-            </button>
-          </form>
-        </center>
-      </dialog>
-      <main onClick={(e) => setModelOpen(false)}>
+      </nav>{" "}
+      <main>
         <div className="container">
-          {linksToShow.length === 0 ? (
-            <center>
-              <h6 class="card-title">Not Found</h6>
-            </center>
-          ) : (
-            linksToShow.map((link) => (
-              <div className="row fullWidth">
-                <div className="col-md-12 fullWidth">
-                  <div className="card  mt-3 mb-3">
-                    {" "}
-                    <div className="card-body">
-                      <center>
-                        <h6 class="card-title">{link.NAME}</h6>
-                        <a href={link.LINK} className="card-link">
-                          Download
-                        </a>
-                      </center>
+          {!modelOpen ? (
+            <React.Fragment>
+              {linksToShow.length === 0 ? (
+                <center>
+                  <h6 class="card-title">Not Found</h6>
+                </center>
+              ) : (
+                linksToShow.map((link) => (
+                  <div className="row fullWidth">
+                    <div className="col-md-12 fullWidth">
+                      <div className="card  mt-3 mb-3">
+                        {" "}
+                        <div className="card-body">
+                          <center>
+                            <h6 class="card-title">{link.NAME}</h6>
+                            <a href={link.LINK} className="card-link">
+                              Download
+                            </a>
+                          </center>
+                        </div>
+                      </div>
                     </div>
+                  </div>
+                ))
+              )}
+            </React.Fragment>
+          ) : (
+            <div className="row fullWidth">
+              <div className="col-md-12 fullWidth">
+                <div className="card  mt-3 mb-3 ">
+                  {" "}
+                  <div className="card-body">
+                    <center>
+                      <h6 class="card-title">Contribute</h6>
+                      <form
+                        action="https://smartforms.dev/submit/5e8c9a23d7d0665194d11f18"
+                        method="POST"
+                        className="form-cls"
+                      >
+                        <label className="mt-2 mb-2 inputWidth">
+                          Have any requests ? Let us know.
+                        </label>
+                        <br />
+                        <label className="mt-2 mb-2 inputWidth">
+                          Have any Links ? Join us now.
+                        </label>
+                        <input
+                          type="text"
+                          name="media"
+                          className="form-control mt-2 mb-2 inputWidth"
+                          id="formGroupExampleInput"
+                          placeholder="Media Name"
+                          onFocus={(e) => (e.target.placeholder = "")}
+                          onBlur={(e) => (e.target.placeholder = "Media Name")}
+                          autocomplete="off"
+                        ></input>
+                        <input
+                          type="text"
+                          name="link"
+                          className=" form-control mt-2 mb-2 inputWidth"
+                          id="formGroupExampleInput"
+                          placeholder="Link ( if Any )"
+                          onFocus={(e) => (e.target.placeholder = "")}
+                          onBlur={(e) =>
+                            (e.target.placeholder = "Link ( if Any )")
+                          }
+                          autocomplete="off"
+                        ></input>
+                        <label className="mt-2 mb-2 inputWidth">
+                          Wanna be notified ? Give you mail id or Social Media
+                          Usernames. W'll ping you onces you are served.
+                        </label>
+                        <input
+                          type="text"
+                          name="link"
+                          className=" form-control mt-2 mb-2 inputWidth"
+                          id="formGroupExampleInput"
+                          placeholder="Contact Id ( Optional )"
+                          onFocus={(e) => (e.target.placeholder = "")}
+                          onBlur={(e) =>
+                            (e.target.placeholder = "Contact Id ( Optional )")
+                          }
+                          autocomplete="off"
+                        ></input>
+                        <button type="submit" className="mx-2 mt-3">
+                          Send
+                        </button>
+                        <button
+                          type="button"
+                          onClick={(e) => setModelOpen(false)}
+                          className="mx-2 mt-3"
+                        >
+                          Close
+                        </button>
+                      </form>
+                    </center>
                   </div>
                 </div>
               </div>
-            ))
+            </div>
           )}
         </div>
       </main>
-      {/* <footer> */}
       <i
         className="fa fa-plus float"
         onClick={(e) => {
@@ -140,7 +148,6 @@ function App() {
           setModelOpen(!modelOpen);
         }}
       ></i>
-      {/* </footer> */}
     </div>
   );
 }
