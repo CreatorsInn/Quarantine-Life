3ed6d0c747e2b5147cc8f6dcfc56b634f21ba5ad
Sivaprakash22 <sivaprakash138@gmail.com>
2020-04-10 00:11:40 +0530

    Button IU updated

--- src/components/App.js
+++ src/components/App.js
@@ -37,116 +37,120 @@ function App() {
         ></input>
       </nav>{" "}
       <main>
-          {!modelOpen ? (
-            <React.Fragment>
-              {linksToShow.length === 0 ? (
-                <center>
-                  <h6 class="card-title">Not Found</h6>
-                </center>
-              ) : (
-                linksToShow.map((link) => (
-                  <div className="row fullWidth">
-                    <div className="col-md-12 fullWidth">
-                      <div className="card  mt-3 mb-3">
-                        <div className="card-body">
-                          <center>
-                            <h6 class="card-title">{link.NAME}</h6>
-                            <a href={link.LINK} className="card-link">
-                              Download
-                            </a>
-                          </center>
-                        </div>
+        {!modelOpen ? (
+          <React.Fragment>
+            {linksToShow.length === 0 ? (
+              <center>
+                <h6 class="card-title">Not Found</h6>
+              </center>
+            ) : (
+              linksToShow.map((link) => (
+                <div className="row fullWidth">
+                  <div className="col-md-12 fullWidth">
+                    <div className="card  mt-3 mb-3">
+                      <div className="card-body">
+                        <center>
+                          <h6 class="card-title">{link.NAME}</h6>
+                          <a href={link.LINK} className="card-link">
+                            Download
+                          </a>
+                        </center>
                       </div>
                     </div>
                   </div>
-                ))
-              )}
-            </React.Fragment>
-          ) : (
-            <div className="row fullWidth">
-              <div className="col-md-12 fullWidth">
-                <div className="card  mt-3 mb-3 ">
-                  {" "}
-                  <div className="card-body">
-                    <center>
-                      <h6 class="card-title">Contribute</h6>
-                      <form
-                        action="https://formsubmit.co/quarantinelife.creators@gmail.com"
-                        method="POST"
-                        className="form-cls"
+                </div>
+              ))
+            )}
+          </React.Fragment>
+        ) : (
+          <div className="row fullWidth">
+            <div className="col-md-12 fullWidth">
+              <div className="card  mt-3 mb-3 ">
+                {" "}
+                <div className="card-body">
+                  <center>
+                    <h6 class="card-title">Contribute</h6>
+                    <form
+                      action="https://formsubmit.co/quarantinelife.creators@gmail.com"
+                      method="POST"
+                      className="form-cls"
+                    >
+                      <label className="mt-2 mb-2 inputWidth">
+                        Have any requests ? Let us know.
+                      </label>
+                      <br />
+                      <label className="mt-2 mb-2 inputWidth">
+                        Have any Links ? Join us now.
+                      </label>
+                      <input
+                        type="text"
+                        name="media"
+                        className="form-control mt-2 mb-2 inputWidth"
+                        id="formGroupExampleInput"
+                        placeholder="Media Name"
+                        onFocus={(e) => (e.target.placeholder = "")}
+                        onBlur={(e) => (e.target.placeholder = "Media Name")}
+                        autocomplete="off"
+                      ></input>
+                      <input
+                        type="text"
+                        name="link"
+                        className=" form-control mt-2 mb-2 inputWidth"
+                        id="formGroupExampleInput"
+                        placeholder="Link ( if Any )"
+                        onFocus={(e) => (e.target.placeholder = "")}
+                        onBlur={(e) =>
+                          (e.target.placeholder = "Link ( if Any )")
+                        }
+                        autocomplete="off"
+                      ></input>
+                      <input
+                        type="hidden"
+                        name="_next"
+                        value={window.location.href}
+                      ></input>
+                      <input
+                        type="hidden"
+                        name="_captcha"
+                        value="false"
+                      ></input>
+                      <label className="mt-2 mb-2 inputWidth">
+                        Wanna be notified ? Give your mail id or Social Media
+                        username . We'll ping you once your request is served.
+                      </label>
+                      <input
+                        type="text"
+                        name="contact"
+                        className=" form-control mt-2 mb-2 inputWidth"
+                        id="formGroupExampleInput"
+                        placeholder="Contact Id ( Optional )"
+                        onFocus={(e) => (e.target.placeholder = "")}
+                        onBlur={(e) =>
+                          (e.target.placeholder = "Contact Id ( Optional )")
+                        }
+                        autocomplete="off"
+                      ></input>
+                      <button
+                        type="submit"
+                        id="submit"
+                        className="mx-2 qu-btn  mt-3"
                       >
-                        <label className="mt-2 mb-2 inputWidth">
-                          Have any requests ? Let us know.
-                        </label>
-                        <br />
-                        <label className="mt-2 mb-2 inputWidth">
-                          Have any Links ? Join us now.
-                        </label>
-                        <input
-                          type="text"
-                          name="media"
-                          className="form-control mt-2 mb-2 inputWidth"
-                          id="formGroupExampleInput"
-                          placeholder="Media Name"
-                          onFocus={(e) => (e.target.placeholder = "")}
-                          onBlur={(e) => (e.target.placeholder = "Media Name")}
-                          autocomplete="off"
-                        ></input>
-                        <input
-                          type="text"
-                          name="link"
-                          className=" form-control mt-2 mb-2 inputWidth"
-                          id="formGroupExampleInput"
-                          placeholder="Link ( if Any )"
-                          onFocus={(e) => (e.target.placeholder = "")}
-                          onBlur={(e) =>
-                            (e.target.placeholder = "Link ( if Any )")
-                          }
-                          autocomplete="off"
-                        ></input>
-                        <input
-                            type="hidden"
-                            name="_next"
-                            value={window.location.href}
-                        ></input>
-                        <input
-                          type="hidden"
-                          name="_captcha"
-                          value="false"
-                        ></input>
-                        <label className="mt-2 mb-2 inputWidth">
-                          Wanna be notified ? Give your mail id or Social Media
-                          username . We'll ping you once your request is served.
-                        </label>
-                        <input
-                          type="text"
-                          name="contact"
-                          className=" form-control mt-2 mb-2 inputWidth"
-                          id="formGroupExampleInput"
-                          placeholder="Contact Id ( Optional )"
-                          onFocus={(e) => (e.target.placeholder = "")}
-                          onBlur={(e) =>
-                            (e.target.placeholder = "Contact Id ( Optional )")
-                          }
-                          autocomplete="off"
-                        ></input>
-                        <button type="submit" id="submit" className="mx-2 mt-3">
-                          Send
-                        </button>
-                        <button
-                          type="button"
-                          onClick={(e) => setModelOpen(false)}
-                          className="mx-2 mt-3"
-                        >
-                          Close
-                        </button>
-                      </form>
-                    </center>
-                  </div>
+                        Send
+                      </button>
+                      <button
+                        type="button"
+                        onClick={(e) => setModelOpen(false)}
+                        className="mx-2  qu-btn  mt-3"
+                      >
+                        Close
+                      </button>
+                    </form>
+                  </center>
                 </div>
               </div>
             </div>
-          )}
+          </div>
+        )}
       </main>
       <i
         className="fa fa-plus float"
